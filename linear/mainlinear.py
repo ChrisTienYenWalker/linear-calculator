d9945d4d5eae4167ea04a4b44affb8b911a1b564
Master4020 <47930042+Master4020@users.noreply.github.com>
2019-05-25 14:38:31 -0400

    finished linear

--- linear/mainlinear.py
+++ linear/mainlinear.py
@@ -1,44 +1,80 @@
-import formula as form 
-import points as points
-x1 = input('Enter X1: ')
-y1 = input('Enter Y1: ')
-x2 = input('Enter X2: ')
-y2 = input('Enter X2: ')
-points.rate(0, y1, y2, x1, x2)
+import linear_using_formula as formula 
+import linear_using_points as points
+
 print('welcome to the linear calculator')
-linearQues = input('Would you like to find [slope, x-int, y-int, formula, descprition, prediction] ')
-linedec = input(str("Do you know the [points or the formula]: "))
-if linedec == 'points' or linedec == 'Points' or linedec == 'point' or linedec == 'Point':
-    x1 = input('Enter X1: ')
-    y1 = input('Enter Y1: ')
-    x2 = input('Enter X2: ')
-    y2 = input('Enter X2: ')
-    if linearQues == 'slope' or linearQues == 'Slope' or linearQues == 's':
-        points.rate(0, y1, y2, x1, x2)
-    if linearQues == 'x-int' or linearQues == 'X-int' or linearQues == 'x':
-        points.x_int(0, y1, y2, x1, x2)
-    if linearQues == 'y-int' or linearQues == 'y' or linearQues == 'Y-int':
-        points.y_int(0, y1, y2,  x1, x2)
-    if linearQues == 'formula' or linearQues == 'Formula' or linearQues == 'f':
-        points.formula(0, y1, y2, x1, x2)
-    if linearQues == 'prediction' or linearQues == 'p' or linearQues == 'Prediction':
-        predict = input(str('Would you like to predict [X or Y]: '))
-        if predict == 'X' or predict == 'x':
-            y3 = input(int('Enter the Y your corrident to find the X: '))
-            points.predictionX(0, y1, y2, x1, x2, y3)
-        if predict == 'Y' or predict == 'y':
-            x3 = input(int('Enter the X your corrident to find the Y: '))
-            points.predictionY(0, y1, y2, x1, x2, x3)
-        if linearQues == 'descprition' or linearQues == 'Descprition' or linearQues == 'd':
+
+while  2 > 1:
+    linearQues = input('Would you like to find [slope, rate, x-int, y-int, formula, descprition, prediction] ')
+    linearQues.lower()
+    linearQues.strip()
+    if linearQues == 'exit':
+        break
+    print('To exit type exit at the end')
+    linedec = input(str('Do you know the [points or the formula]: '))
+    linedec.lower()
+    linedec.strip()
+    if linedec == 'exit':
+        break
+    if linedec == 'points':
+        x1 = int(input('Enter X1: '))
+        y1 = int(input('Enter Y1: '))
+        x2 = int(input('Enter X2: '))
+        y2 = int(input('Enter Y2: '))
+        if linearQues == 'slope' or linearQues == 'rate':
+            print(points.rate(0, y1, y2, x1, x2))
+        if linearQues == 'x-int':
+            print(points.x_int(0, y1, y2, x1, x2))
+        if linearQues == 'y-int':
+            print(points.y_int(0, y1, y2,  x1, x2))
+        if linearQues =='formula':
+            print(points.formula(0, y1, y2, x1, x2))
+        if linearQues == 'perdiction':
+            predict = input(str('Would you like to predict [X or Y]: '))
+            predict.lower()
+            predict.strip()
+            if predict == 'x':
+                y3 = input(int('Enter the Y your corrident to find the X: '))
+                print(points.predictionX(0, y1, y2, x1, x2, y3))
+            if predict =='y':
+                x3 = input(int('Enter the X your corrident to find the Y: '))
+                print(points.predictionY(0, y1, y2, x1, x2, x3))
+        if linearQues == 'desprition':
             des = (points.description.origin(0, y1, y2, x1, x2), points.description.steep(0, y1, y2, x1, x2), points.description.inc(0, y1, y2, x1, x2))
             desit = iter(des)
             print(next(desit))
             print(next(desit))
             print(next(desit))
-if linedec is 'formula' or linedec is 'Formula' or linedec is 'f':
-    print('')
+    if linedec == 'formula':
+        print('If value unknow type 0')
+        input('The formula is y=mx+b')
+        m = int(input('Enter m value: '))
+        b = int(input('Enter b value: '))
+        if linearQues == 'slope' or linearQues == 'rate':
+            print('The slope is ' + m)
+        if linearQues == 'y-int':
+            print('The y-int is ' + b)
+        if linearQues == 'x-int':
+            print('The x-int is ' + formula.x_int(0, m, b))
+        if linearQues == 'prediction':
+            predict == str(input('What would you like to find [X/Y]: '))
+            predict.lower()
+            predict.strip()
+            if predict == 'x':
+                y = int(input('What is the value of Y: '))
+                print('X is ' + formula.predictX(0, m, b, y))
+            if predict == 'y':
+                x = int(input('What is the value of X: '))
+                print('Y is ' + formula.predictY(0, m, b, x))
+        if linearQues == 'descprition':
+            if 1 >= m >= -1:
+                print("not very steep")
+            else:
+                print("Steep")
+            if b = 0:
+                print('Goes through the origin')
+            else:
+                print('Does not go through the origin')
+            
 
 
-                    
-                
 
